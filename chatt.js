219be74bb256a8f9d0ebfa26e11c2ec842019aee
Klaral <klaral@users.noreply.github.com>
2017-05-09 10:54:46 +0200

    ny

--- chatt.js
+++ chatt.js
@@ -1,78 +1,95 @@
-window.onload = function(){
+window.onload = function(){"use strict";
 
-
-var submit = document.getElementById('submit');
-var firstN = document.getElementById('firstN');
-var lastN = document.getElementById('lastN');
+var allName = document.getElementById('allName');
+var loggin = document.getElementById("loggin");     
+var loggut = document.getElementById("loggut");
+var paragraf = document.getElementById("paragraf");
 var text = document.getElementById("text");    
-var table = document.getElementById("table");
-var view = document.getElementById("view");    
-var allName = firstN.value + " " + lastN.value;
-    
-    
+var submit = document.getElementById('submit'); 
+                           
+var table = document.getElementById("table");                           
+
+
+
+var nyttmsg = 0;
+var user = localStorage.getItem("allName");    
+
+
  /***loggin**/
     
-loggin.addEventListener('click',function(){
-  localStorage.setItem('whatever', allName);
-    if (localStorage.getItem('whatever') !== null){
-        var paragraf = document.getElementById("paragraf");
-        paragraf.innerHTML = "Välkommen: " + localStorage.getItem('whatever');  
-    } else{
-        var paragraf = document.getElementById("paragraf");
-        paragraf.innerHTML = "Skriv in ditt namn" + localStorage.getItem('whatever');
-    }
-  
-  var fb = firebase.database();
-    fb.ref().child("message").once("value",function(snapshot){
+
+loggin.addEventListener('click',function (event){
+    user = allName.value;
+    paragraf.innerHTML = "Välkommen" + user + "!";
+    localStorage.setItem('allName', user);
+    allName.value="";
+    loggin.disabled = true;
+    
+});    
+    
+    
+ /**loggut**/
+    
+
+loggut.addEventListener("click", function(event){
+localStorage.removeItem('allName', user);
+    paragraf.innerHTML = "Vänligen skriv ditt namn";
+    loggin.disabled = false;
+}); 
+    
+    
+
+
+ /**meddelande**/
+    
+submit.addEventListener("click", function(event){
+    console.log("Text: " + text.value + "");
+    
+    nyttmsg++;
+    var time = new Date(new Date().getTime()).toLocaleString();
+    
+   var fb = firebase.database();
+    
+    fb.ref("message/" + nyttmsg).set({
+    name: user,
+    meddelande: text.value,
+    id: nyttmsg,
+    time: time /*moment().format('MMMM Do YYYY, h:mm:ss a'), // March 21st 2017, 12:38:42 pm*/
+      
+    })
+    
+    /*text.value = "";*/        /*fb.ref('message/' + message.id).set(message);*/
+    
+fb.ref().child("message").once("value",function(snapshot){
   var data = snapshot.val();
   var count = Object.keys(data).length;
-  var message = {
-      name: allName,
-      text: text.value,
-      time: moment().format('MMMM Do YYYY, h:mm:ss a'), // March 21st 2017, 12:38:42 pm
-      id: count+1
-      
-  } 
   
-  fb.ref('message/' + message.id).set(message);
-
     });
-  
-    
-});
-
-
-view.addEventListener("click", function(event){
-table.innerHTML = "";
-    firebase.database().ref('message/').once('value', function(snapshot) {
-	var allData = snapshot.val();
-        Object.keys(allData).reverse().forEach(function(key){
-            var meddelande = allData[key];
-            
-    var tr = document.createElement("tr");
-            tr.innerHTML = `<td>${meddelande.name}</td>
-<td>${meddelande.text}</td> <td>${meddelande.time}</td> <td>${meddelande.id}</td>`
-            table.appendChild(tr);
-        })
-	
-});
-
-});
-
-    
-    
-    
-    
-/**loggut**/
-    
-var loggaut = document.getElementById("loggaut")
-loggut.addEventListener("click", function(event){
-localStorage.removeItem("whatever");
-    paragraf.innerHTML = "Vänligen skriv ditt namn";
     
 }); 
     
-
-
     
+    /**visa meddelande**/
+    
+submit.addEventListener("click", function(event){
+    table.innerHTML = "";
+    
+    firebase.database().ref('message/').once('value', function(snapshot) {
+	var allData = snapshot.val();
+	   Object.keys(allData).reverse().forEach(function(key){
+    var message = allData[key];
+           
+    var tr = document.createElement("tr");
+tr.innerHTML = `<td>${message.name}</td>
+<td>${message.meddelande}</td> <td>${message.time}</td> <td>${message.id}</td>`
+            table.appendChild(tr);
+           /*message.name + "<td>" + message.meddelande + "<td>" + message.time + "<td>" + message.id;
+           table.appendChild(tr);*/
+           
+        
+        }) 
+    });
+    
+});
 }
+  
